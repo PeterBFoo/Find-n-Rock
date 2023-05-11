4fd2839d2f3bb0a4d2ed5972f86220a5e1b5b873
PeterBFoo <pborrasexposito@cifpfbmoll.eu>
2023-05-11 19:34:30 +0200

    feat: able to sign up in the app

--- src/app/views/signup/signup.component.ts
+++ src/app/views/signup/signup.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { MusicGenre } from 'src/app/services/interfaces/MusicGenreInterface';
+import { MusicGenreService } from 'src/app/services/musicGenre/music-genre.service';
+import { UserService } from 'src/app/services/user/user.service';
+import { CountriesService } from 'src/app/util/services/countries.service';
+import { CountriesData } from 'src/app/util/services/interfaces/CountriesInterface';
+import { Country } from 'src/app/util/services/interfaces/CountryInterface';
+import { catchError } from 'rxjs';
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +14,134 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent {
+  entrepreneur: boolean = false;
+  musicGroup: boolean = true;
 
+  username: string = "";
+  password: string = "";
+  description: string = "";
+  name: string = "";
+  email: string = "";
+  address: string = "";
+  phoneNumber: string = "";
+  country: string = "";
+  integrants: string = "";
+  image: string = "";
+  musicGenres: MusicGenre[] = [];
+  dataloaded: boolean = false;
+
+  availableCountries: Country[] = [];
+  availableMusicGenres: MusicGenre[] = [];
+  errorMessage: string = "";
+
+  constructor(private musicGenreService: MusicGenreService, private countryService: CountriesService, private userService: UserService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.musicGenreService.getAllMusicGenres().subscribe((musicGenres: MusicGenre[]) => {
+      this.availableMusicGenres = musicGenres;
+    });
+
+    this.countryService.getAllCountries().subscribe((countriesData: CountriesData) => {
+      this.availableCountries = countriesData.data;
+      this.dataloaded = true;
+    });
+  }
+
+  register() {
+    try {
+      this.validateFields();
+
+      if (this.entrepreneur) {
+        this.userService.signup({
+          username: this.username,
+          password: this.password,
+          name: this.name,
+          email: this.email,
+          address: this.address,
+          phone: this.phoneNumber,
+          country: this.country,
+          image: this.image,
+          description: this.description,
+          role: "entrepreneur"
+        }).pipe(
+          catchError((error: any) => {
+            this.errorMessage = error.error;
+            console.log(this.errorMessage);
+            throw error;
+          }))
+          .subscribe((response: any) => {
+            this.router.navigate(['/login']);
+          });
+      } else {
+        this.userService.signup({
+          username: this.username,
+          password: this.password,
+          name: this.name,
+          email: this.email,
+          address: this.address,
+          phone: this.phoneNumber,
+          country: this.country,
+          image: this.image,
+          description: this.description,
+          integrants: this.integrants,
+          musicGenres: this.musicGenres,
+          role: "group"
+        }).pipe(
+          catchError((error: any) => {
+            this.errorMessage = error.error;
+            throw error;
+          }))
+          .subscribe((response: any) => {
+            this.router.navigate(['/login']);
+          });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
+
+  selectGenre($event: any) {
+    if (this.musicGenres.includes($event.target.value)) {
+      let i = this.musicGenres.indexOf($event.target.value);
+      this.musicGenres.splice(i, 1);
+      // remove atribute "selected"
+      $event.target.checked = false;
+    } else {
+      this.musicGenres.push($event.target.value);
+      $event.target.checked = true;
+    }
+    console.log(this.musicGenres);
+  }
+
+  selectRole(event: any) {
+    if (event.target.value == "Entrepreneur") {
+      this.entrepreneur = true;
+      this.musicGroup = false;
+    } else {
+      this.entrepreneur = false;
+      this.musicGroup = true;
+    }
+  }
+
+  onSelectCountry(event: any) {
+    this.country = event.target.value;
+  }
+
+  validateFields() {
+    let mandatoryFields = ["username", "password", "name", "email", "address", "phoneNumber", "country", "image", "description"];
+    let mandatoryFieldsGroup = ["integrants", "musicGenres"];
+
+    if (this.musicGroup) {
+      mandatoryFields = mandatoryFields.concat(mandatoryFieldsGroup);
+    }
+
+    mandatoryFields.forEach((field: string) => {
+      let component: any = this;
+      if (component[field] == "") {
+        this.errorMessage = "Please fill all the fields, " + field + " is missing";
+        throw this.errorMessage;
+      }
+    });
+  }
 }

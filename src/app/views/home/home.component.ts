06a620006c40dd96da40974510d18f2a527b06e5
PeterBFoo <pborrasexposito@cifpfbmoll.eu>
2023-05-11 19:32:17 +0200

    feat: shows post and header with user data

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,31 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Post } from '../../services/interfaces/PostInterface';
-import { PostService } from '../../services/post-service.service';
-import { catchError } from 'rxjs/operators';
-import { throwError } from 'rxjs';
+import { PostService } from '../../services/post/post-service.service';
+import { User } from '../../services/interfaces/UserInterface';
+import { UserService } from '../../services/user/user.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  posts: Post[] = [];
+  posts!: Post[];
+  currentUser!: User;
+  loadedData: boolean = false;
 
-  constructor(private postService: PostService, private router: Router) { }
+  constructor(private postService: PostService, private userService: UserService, private router: Router) {
+  }
 
   ngOnInit(): void {
-    this.postService.getAllPosts().pipe(
-      catchError((err) => {
-        if (err.status === 401) {
-          this.router.navigate(['/login']);
-        }
-        throw err;
-      })
-    ).subscribe((posts: any) => {
+    if (!this.userService.getToken()) {
+      this.router.navigate(['/login']);
+    }
+
+    if (this.userService.getUser() == null) {
+      this.userService.getProfile().subscribe((user: User) => {
+        this.currentUser = user;
+        this.userService.setUser(user);
+        console.log(this.userService.getUser());
+      });
+    } else {
+      this.currentUser = this.userService.getUser();
+      this.loadedData = true;
+    }
+
+    this.postService.getAllPosts().subscribe((posts: Post[]) => {
       this.posts = posts;
+      this.loadedData = true;
     });
   }
 }

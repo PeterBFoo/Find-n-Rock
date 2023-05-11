8770883451412513a062c1a5c8ef84068d516b3a
PeterBFoo <pborrasexposito@cifpfbmoll.eu>
2023-05-11 19:34:09 +0200

    feat: able to do login with an existing user

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +9,28 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  username: string = "";
+  password: string = "";
+  errorMessage: string = "";
+
+  constructor(private userService: UserService, private router: Router) { }
+
+  async onLogin() {
+    this.validateInput(this.username, this.password);
+    if (this.errorMessage != "") return;
+
+    this.userService.login(this.username, this.password).subscribe((res) => {
+      if (res.token) {
+        this.userService.setToken(res.token);
+        this.userService.setUser(res.user);
+        this.router.navigate(["/home"]);
+      } else {
+        this.errorMessage = res.error;
+      }
+    });
+  }
+
+  validateInput(username: string, password: string) {
+    this.errorMessage = username == "" || password == "" ? "Please fill in all fields" : "";
+  }
 }

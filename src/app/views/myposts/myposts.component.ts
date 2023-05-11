9d13b54a882bb667e8ede5451086c0ad8c11c0df
PeterBFoo <pborrasexposito@cifpfbmoll.eu>
2023-05-11 19:32:44 +0200

    feat: new view to see the user's posts

---
+++ src/app/views/myposts/myposts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-myPosts',
+  templateUrl: './myposts.component.html',
+  styleUrls: ['./myposts.component.scss']
+})
+export class MyPostsComponent {
+
+}

a77ae0b43c20dca1960a0dfa0a2d37127d1307f7
PeterBFoo <pborrasexposito@cifpfbmoll.eu>
2023-05-11 19:27:51 +0200

    feat: header with user information

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/services/interfaces/UserInterface';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +10,47 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  @Input() user!: User;
+  username: string = "";
+  userimage: string = "";
+  isGroup: boolean = false;
+  isEntrepreneur: boolean = false;
+
+  menuIsOpen: boolean = false;
+  menu: HTMLDivElement | null = null;
+
+
+  constructor(private userService: UserService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.username = this.user?.username || "";
+    this.userimage = this.user?.image || "";
+    this.isGroup = this.user?.role.canSubscribe || false;
+    this.isEntrepreneur = this.user?.role.canManagePosts || false;
+
+    this.menu = document.getElementById("menu") as HTMLDivElement;
+  }
+
+  openMenu() {
+    this.menu!.style.transform = "translateX(0%)";
+  }
+
+  closeMenu() {
+    this.menu!.style.transform = "translateX(100%)";
+  }
+
+  onToggleMenu() {
+    if (this.menuIsOpen) {
+      this.closeMenu();
+      this.menuIsOpen = false;
+    } else {
+      this.openMenu();
+      this.menuIsOpen = true;
+    }
+  }
+
+  onLogout() {
+    this.userService.logout();
+    this.router.navigate(['/login']);
+  }
 }

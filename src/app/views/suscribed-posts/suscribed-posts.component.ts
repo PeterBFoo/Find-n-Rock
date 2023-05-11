19585be56267245e2d531b7c076071b8a1b39e04
PeterBFoo <pborrasexposito@cifpfbmoll.eu>
2023-05-11 19:33:33 +0200

    feat: new view to see posts where user is suscribed

---
+++ src/app/views/suscribed-posts/suscribed-posts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-suscribed-posts',
+  templateUrl: './suscribed-posts.component.html',
+  styleUrls: ['./suscribed-posts.component.scss']
+})
+export class SuscribedPostsComponent {
+
+}

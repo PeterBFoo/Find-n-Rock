e51d4cf49a5ed961c16ab4fdd8da9f0d7786c927
PeterBFoo <pborrasexposito@cifpfbmoll.eu>
2023-05-13 22:44:00 +0200

    feat: add more routing paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,22 @@ import { HomeComponent } from './views/home/home.component';
 import { LoginComponent } from './views/login/login.component';
 import { SignupComponent } from './views/signup/signup.component';
 import { PostViewComponent } from './views/post-view/post-view.component';
+import { ProfileComponent } from './views/profile/profile.component';
+import { MyPostsComponent } from './views/myposts/myposts.component';
+import { CreatePostComponent } from './views/create-post/create-post.component';
+import { SuscribedPostsComponent } from './views/suscribed-posts/suscribed-posts.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'post/:id', component: PostViewComponent }
+  { path: 'post/:id', component: PostViewComponent },
+  { path: 'user/profile', component: ProfileComponent },
+  { path: 'user/posts', component: MyPostsComponent },
+  { path: 'user/posts/suscribed', component: SuscribedPostsComponent },
+  { path: 'posts/create', component: CreatePostComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+
 ];
 
 @NgModule({

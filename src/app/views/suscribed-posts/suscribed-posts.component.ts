65a3715727120ce6aedc7ea7cc1994f01534e8a2
PeterBFoo <pborrasexposito@cifpfbmoll.eu>
2023-05-22 22:42:28 +0200

    feat: suscriptions view

--- src/app/views/suscribed-posts/suscribed-posts.component.ts
+++ src/app/views/suscribed-posts/suscribed-posts.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Post } from 'src/app/services/interfaces/PostInterface';
+import { User } from 'src/app/services/interfaces/UserInterface';
+import { PostService } from 'src/app/services/post/post-service.service';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-suscribed-posts',
@@ -6,5 +11,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./suscribed-posts.component.scss']
 })
 export class SuscribedPostsComponent {
+  posts!: Post[];
+  currentUser!: User;
+  loadedData: boolean = false;
 
+  postsNotFoundImage: string = 'assets/images/posts_not_found.jpeg';
+
+  constructor(private postService: PostService, private userService: UserService, private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.currentUser = this.userService.getUser();
+
+    this.postService.getSuscribedPosts().subscribe((posts: Post[]) => {
+      this.posts = posts;
+      this.loadedData = true;
+    });
+  }
+
+  unsuscribedEvent(postId: any) {
+    this.postService.unsuscribeToPost(postId).subscribe((post: Post) => {
+      this.postService.getSuscribedPosts().subscribe((posts: Post[]) => {
+        this.posts = posts;
+      });
+    });
+
+  }
 }

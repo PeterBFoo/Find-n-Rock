a94eea80b05978692a406b44ed48a2a0946ab818
PeterBFoo <pborrasexposito@cifpfbmoll.eu>
2023-06-02 00:26:23 +0200

    fix: environment issues

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
     production: true,
     apiUrl: 'https://findnrock.tech/api/api',
     countriesUrl: 'https://wft-geo-db.p.rapidapi.com/v1/geo/countries',
-    apiKey: env.COUNTRIES_SERVICE_API_KEY || process.env['COUNTRIES_SERVICE_API_KEY'],
-    apiHost: env.COUNTRIES_SERVICE_API_HOST || process.env['COUNTRIES_SERVICE_API_HOST']
+    apiKey: env.COUNTRIES_SERVICE_API_KEY,
+    apiHost: env.COUNTRIES_SERVICE_API_HOST
 };

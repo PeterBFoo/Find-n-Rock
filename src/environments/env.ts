12e8ed21263679c8436b15c4d533323d069e31bb
PeterBFoo <pborrasexposito@cifpfbmoll.eu>
2023-06-01 19:00:41 +0200

    fix: env file

--- src/environments/env.ts
+++ src/environments/env.ts
@@ -1,7 +1,4 @@
-import * as dotenv from "dotenv";
-dotenv.config({ path: __dirname + '/.env' });
-
 export const env = {
-    COUNTRIES_SERVICE_API_KEY: process.env["COUNTRIES_SERVICE_API_KEY"],
-    COUNTRIES_SERVICE_API_HOST: process.env["COUNTRIES_SERVICE_API_HOST"]
+    COUNTRIES_SERVICE_API_KEY: '516cd590b4mshd61ecf6bead6f7dp12b432jsn1fcbe0fca825',
+    COUNTRIES_SERVICE_API_HOST: 'wft-geo-db.p.rapidapi.com'
 }

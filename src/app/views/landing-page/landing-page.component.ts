8efaac8a9528b350354c625f6fbb3ba9c8ef0802
PeterBFoo <pborrasexposito@cifpfbmoll.eu>
2023-06-01 23:18:27 +0200

    refactor: wait to render video to display view

--- src/app/views/landing-page/landing-page.component.ts
+++ src/app/views/landing-page/landing-page.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss']
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements AfterViewInit {
+  renderedVideo: boolean = false;
+  landingVideo: string = "assets/videos/landing-video.mp4";
 
+  ngAfterViewInit(): void {
+    this.renderedVideo = true;
+  }
 }
